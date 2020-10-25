a6cc35bc77c9d9fb826d8c700a696ac0afa4df82
Jemil <jemil.pepena@griffithuni.edu.au>
2020-10-25 16:46:21 +1000

    title format

--- gui.py
+++ gui.py
@@ -168,8 +168,7 @@ class listingPanel( wx.ScrolledWindow):
                         gb.Add(val, (q + 10, 10)) 
                         break
 
-            else:
-               
+            else: 
 
                 val = wx.StaticText(self, label= desc)
                 val.SetFont(font_details)
@@ -332,19 +331,40 @@ class MyFrame(wx.Frame):
                     word =  wx.TextEntryDialog(self, 'Enter Word')
                     word.SetValue("Pool")
                     if word.ShowModal() == wx.ID_OK:
-                        s = str(word.GetValue())                         
+                        user_input = str(word.GetValue())                         
                        
-                        result = report_listing_info.keyword(s, str(self.start_date), str(self.end_date), 1)   
+                        result = report_listing_info.keyword(user_input, str(self.start_date), str(self.end_date), 1)   
                         if len(result) != 0:   
-                            if len(self.start_date) != 0 and len(self.end_date) != 0:
-                                title = f'Listings with {s} from {self.start_date} to {self.start_date}'
+                            if len(user_input) == 0:
+                                title = 'Listings in all of Sydney'
+                                if len(self.start_date) != 0 and len(self.end_date):
+                                    title = f'Listings in all of Sydney from {self.start_date} to {self.end_date}'
                             else:
-                                title = f'All Listings with {s}'              
-                            results_(self, result, title, s, 0,'Keyword',self.start_date,self.end_date)
+                                user_input = user_input.split(',')
+                                if len(user_input) == 1:
+                                    user_input = user_input[0][0].upper() + user_input[0][1:]
+                                else:
+                                    user_input_copy = user_input
+                                    user_input = str()
+                                    for k in range(len(user_input_copy)):
+                                        user_input_copy[k] = user_input_copy[k].lower()
+                                        user_input_copy[k] = user_input_copy[k][0].upper() + user_input_copy[k][1:]
+                                        if k != len(user_input_copy) - 1 and k != 0:
+                                            user_input += ', ' + user_input_copy[k] 
+                                        elif k == 0:
+                                            user_input += user_input_copy[k]
+                                        else:
+                                            user_input +=  ' and ' + user_input_copy[k]
+
+                            if len(self.start_date) != 0 and len(self.end_date) != 0:
+                                title = f'Listings with {user_input} from {self.start_date} to {self.start_date}'
+                            else:
+                                title = f'All Listings with {user_input}'              
+                            results_(self, result, title, user_input, 0,'Keyword',self.start_date,self.end_date)
                             self.Close()
                         else:
                             error =  wx.TextEntryDialog(self, 'No results for Keyword:')
-                            error.SetValue(f'{s}')
+                            error.SetValue(f'{user_input}')
                             error.ShowModal()
 
         
